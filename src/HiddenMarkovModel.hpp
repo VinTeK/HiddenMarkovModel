93bd5df01b5fce373f3fec7c573368a46b24d1b8
Sang Han <VinTeK1@gmail.com>
2015-04-06 18:06:48 -0400

    add Utils.hpp to separate utility functionality

--- src/HiddenMarkovModel.hpp
+++ src/HiddenMarkovModel.hpp
@@ -75,14 +75,4 @@ private:
 };
 
 
-/*
- * Utility functions
- */
-
-/** Return a vector of this line split into space delimited words. */
-template <typename T> std::vector<T> split(const std::string& line);
-/** Return vector of observation sequences in an .obs file. */
-std::vector<std::vector<std::string> > parseObsFile(const std::string& filename);
-
-
 #endif
