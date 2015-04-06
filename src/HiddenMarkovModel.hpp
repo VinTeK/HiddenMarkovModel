b69909551f6ae4faa7c15260262feb7b90fc7f8a
Sang Han <VinTeK1@gmail.com>
2015-04-06 18:00:02 -0400

    add more documentation to HMM interface

--- src/HiddenMarkovModel.hpp
+++ src/HiddenMarkovModel.hpp
@@ -14,31 +14,55 @@ public:
 	size_t numOfStates() const { return _numOfStates; }
 	size_t numOfOutputs() const { return _numOfOutputs; }
 
-	/** Return state transition probability from states stt1 to stt2. */
+	/**
+	 * Return state transition probability from states stt1 to stt2.
+	 * @param stt1 source state
+	 * @param stt2 destination state
+	 */
 	double transition(const std::string& stt1, const std::string& stt2);
-	/** Return observation emission probability of output out in state stt. */
+	/**
+	 * Return observation emission probability of output out in state stt.
+	 * @param stt current state
+	 * @param out the output symbol observed at this state
+	 */
 	double emission(const std::string& stt, const std::string& out);
-	/** Return initial state probability of state stt. */
+	/**
+	 * Return initial state probability of state stt.
+	 * @param stt current state
+	 */
 	double initState(const std::string& stt);
 
-	/** Returns initial probability of starting in a state. */
+	/**
+	 * Returns initial probability of starting in a state.
+	 */
 	double initEval(const std::string& out, const std::string& stt);
-	/** Returns probability of a single output symbol and a state transition. */
+	/**
+	 * Returns probability of a single output symbol and a state transition.
+	 */
 	double eval(const std::string& out, const std::string stts[2]);
-	/** Returns probability of an output sequence based on a given state sequence. */
+	/**
+	 * Returns probability of an output sequence based on a given state sequence.
+	 */
 	double eval(const std::vector<std::string>& out, const std::vector<std::string>& stt);
 
-	/** Returns the forward variables for each observation sequence in a given .obs file. */
+	/**
+	 * Returns the forward variables for each observation sequence in a given .obs file.
+	 */
 	std::vector<double> forward(const std::string& filename);
-	/** Returns the backward variables for each observation sequence in a given .obs file. */
+	/**
+	 * Returns the backward variables for each observation sequence in a given .obs file.
+	 */
 	std::vector<double> backward(const std::string& filename);
-	/** Returns the most likely state sequence for each observation sequence in an .obs file. */
+	/**
+	 * Returns the pair of the most likely state sequence probability and its actual state path
+	 * for each observation sequence in a given .obs file.
+	 */
 	std::vector<std::pair<double, std::vector<std::string> > > viterbi(const std::string& filename);
 
 private:
-	double forwardHelper(const std::vector<std::string>& obs, int t, const std::string& curStt);
-	double backwardHelper(const std::vector<std::string>& obs, int t, const std::string& curStt);
-	std::pair<double, std::vector<std::string> > viterbiHelper(const std::vector<std::string>& obs);
+	double forwardHelper(const std::vector<std::string>&, int, const std::string&);
+	double backwardHelper(const std::vector<std::string>&, int, const std::string&);
+	std::pair<double, std::vector<std::string> > viterbiHelper(const std::vector<std::string>&);
 
 private:
 	size_t _numOfStates, _numOfOutputs;
